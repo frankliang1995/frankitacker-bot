315e5a354853cdfa6022f9c1089113144cb180f9
frankliang1995 <frankliang@trentu.ca>
2021-06-28 23:59:16 -0400

    worked on member update

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ INITIAL_EXTENSIONS = [
 class FrankBot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix='!',
-                         Intents=discord.Intents(members=True, presences=True))
+                         intents=discord.Intents(members=True, presences=True)
+                         )
         self.client_id = config('CLIENT_ID')
         self.token = config('TOKEN')
 
@@ -27,9 +28,9 @@ class FrankBot(commands.Bot):
 
     # Let us know the bot is ready
     async def on_ready(self):
-        self.intents.presences = True
         # self.session = aiohttp.ClientSession(loop=self.loop)
         print('We have logged in as ' + self.user.name)
+
 
     # Gets message from client
     async def on_message(self, message):
@@ -45,8 +46,32 @@ class FrankBot(commands.Bot):
                 msg.content = "{}tag get {}".format(ctx.prefix, new_content)
                 await self.process_commands(msg)
 
+    # Get update from member
     async def on_member_update(self, before, after):
-        print(after.member.status)
+        if before is not after:
+            # current streaming status
+            if after.status == "streaming":
+                # checks if the member has a ttv role
+                ttv = discord.utils.find(lambda x: x.name == "ttv", after.guild.roles)
+                if ttv in after.roles:
+                    # checks if the member has the streaming role
+                    streaming = discord.utils.find(lambda x: x.name == "🔴 Streaming", after.guild.roles)
+                    if streaming not in after.roles:
+                        # add the user to the streaming
+                        after.add_roles(streaming)
+            # checks if the streaming role was recently added to the after member
+            streaming = discord.utils.find(lambda x: x.name == "🔴 Streaming", after.guild.roles)
+            if streaming in after.roles and streaming not in before.roles:
+                # sends streaming message to
+                streaming_channel = discord.utils.find(lambda x: x.name == "streaming", after.guild.channels)
+                # await streaming_channel.send(f"{after.name} is now live on {}")
+
+
+
+
+
+
+        # print(after.member.status)
 
     # Closes the session
     async def close(self):
